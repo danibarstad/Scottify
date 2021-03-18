595b21c24d25eac2e0ac0edb8051b3bd8199795a
dani <danibarstad@gmail.com>
2021-03-17 22:05:44 -0500

    setting up access_token function

---
+++ scripts.js
@@ -0,0 +1,15 @@
+let button = document.getElementById('btn');
+
+let artist = document.getElementById('artist');
+let song = document.getElementById('song');
+let album = document.getElementById('album');
+
+button.addEventListener('click', getSong);
+
+function getAccess() {
+
+}
+
+function getSong(access_token) {
+
+}
