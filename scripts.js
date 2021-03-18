ccb251c93429f4c59f877178d07e414725345a52
dani <danibarstad@gmail.com>
2021-03-17 23:12:50 -0500

    gained access token

--- scripts.js
+++ scripts.js
@@ -1,15 +1,35 @@
 let button = document.getElementById('btn');
 
+let redirectUri = 'http://127.0.0.1:5500/index.html';
+let scopes = 'user-read-private playlist-read-private';
+
 let artist = document.getElementById('artist');
 let song = document.getElementById('song');
 let album = document.getElementById('album');
 
-button.addEventListener('click', getSong);
+button.addEventListener('click', () => {
+    let acc_tok = getAccess();
+    getSong(acc_tok);
+});
 
 function getAccess() {
-
+    const responseType = 'token';
+    const CLIENT_ID;
+    let url = `https://accounts.spotify.com/authorize?response_type=${responseType}&client_id=${CLIENT_ID}&scope=${scopes}&redirect_uri=${redirectUri}`;
+    window.location = url;
+    return getHashParams();
 }
 
-function getSong(access_token) {
+function getSong(a_t) {
+    console.log(a_t);
+}
 
+function getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    while ( e = r.exec(q)) {
+        hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams.access_token;
 }
