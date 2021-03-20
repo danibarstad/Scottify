fc0d328971b9bd418319f9916e49c49ba4ea3e49
dani <danibarstad@gmail.com>
2021-03-20 14:48:46 -0500

    moved html and css to public folder

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 let login = document.getElementById('login');
 let track = document.getElementById('track');
 let artist = document.getElementById('artist');
-let redirectUri = 'http://127.0.0.1:5500/index.html';
+let redirectUri = 'http://127.0.0.1:5500/public/index.html';
 let scopes = 'user-read-private playlist-read-private';
 
 window.addEventListener('load', function() {
